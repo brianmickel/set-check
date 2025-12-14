2e88b1d4ccfbe5cdfe0e58363cc891be81159f0c
Brian Mickel <brianemickel@gmail.com>
2025-12-14 15:52:09 -0700

    list set cards as buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// 1,2,3
+// D,S,O
+// G,P,R
+// S,L,E (Solid, Striped, Empty)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const cards: string[] = [];
+for (const n of ["1", "2", "3"]) {
+  for (const s of ["Diamond", "Oval", "Squiggle"]) {
+    for (const c of ["Red", "Green", "Purple"]) {
+      for (const f of ["Solid", "Striped", "Empty"]) {
+        cards.push(`${n}-${s}-${c}-${f}`);
+      }
+    }
+  }
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <h1>Set Check</h1>
+      <p>UNDER CONSTRUCTION</p>
+      {cards.map((c) => (
+        <div>
+          <button>{c}</button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
